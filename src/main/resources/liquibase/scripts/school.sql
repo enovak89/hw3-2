7100cf6fe1123872df70eac3dce57d708be9e147
enovak89 <enovak89@mail.ru>
2023-05-03 22:49:52 +0300

    point 17

--- src/main/resources/liquibase/scripts/school.sql
+++ src/main/resources/liquibase/scripts/school.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 
-    -- changeset novak:1
+    -- changeset novak:create_index_student_name
 CREATE INDEX student_name_index ON student (name);
 
--- changeset novak:2
+-- changeset novak:create_index_faculty_name_color
 CREATE INDEX faculty_name_color_index ON faculty (name, color);
