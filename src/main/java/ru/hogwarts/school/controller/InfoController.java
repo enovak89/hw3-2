69a04a6f4c5307278caea45301d41b58b127914e
Работник <enovak89@yandex,ru>
2023-05-04 14:23:39 +0300

    point 18

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,13 +1,14 @@
 package ru.hogwarts.school.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
-
-    @Value("${server.port}")
+    @Value("${local.server.port}")
     Integer port;
 
     @GetMapping("/get-port")
